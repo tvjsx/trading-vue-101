786c9700982b6723c15f97e1664f9ec562392796
C451 <codeallright@protonmail.com>
2019-03-25 13:38:30 +0300

    Let's Rock

---
+++ README.md
@@ -0,0 +1,1 @@
+## Example project for **Getting started** guide. 
---
+++ src/App.vue
@@ -0,0 +1,44 @@
+<template>
+
+<!-- TradingVueJs 101 (example from 'Getting Started' ) -->
+
+<trading-vue :data="chart" :width="this.width" :height="this.height"
+    :color-back="colors.colorBack"
+    :color-grid="colors.colorGrid"
+    :color-text="colors.colorText">
+</trading-vue>
+</template>
+
+<script>
+import TradingVue from 'trading-vue-js'
+import Data from '../data/data.json'
+
+export default {
+    name: 'app',
+    components: { TradingVue },
+    methods: {
+        onResize(event) {
+            this.width = window.innerWidth
+            this.height = window.innerHeight
+        }
+    },
+    mounted() {
+        window.addEventListener('resize', this.onResize)
+    },
+    beforeDestroy() {
+        window.removeEventListener('resize', this.onResize)
+    },
+    data() {
+        return {
+            chart: Data,
+            width: window.innerWidth,
+            height: window.innerHeight,
+            colors: {
+                colorBack: '#fff',
+                colorGrid: '#eee',
+                colorText: '#333',
+            }
+        };
+    }
+};
+</script>
