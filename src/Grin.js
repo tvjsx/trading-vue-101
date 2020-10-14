1698d6c4504c69d9337d26f8d1863390e0c9064a
C451 <codeallright@protonmail.com>
2020-10-14 17:30:48 +0300

    Update grin bro

--- src/Grin.js
+++ src/Grin.js
@@ -1,3 +1,4 @@
+// Grin, your overlay friend
 
 // Adds all necessary stuff for you.
 import { Overlay } from 'trading-vue-js'
@@ -7,7 +8,10 @@ export default {
     mixins: [Overlay],
     methods: {
         meta_info() {
-            return { author: 'C451', version: '1.0.0' }
+            return {
+                author: 'C451',
+                version: '2.0.0'
+            }
         },
         // Here goes your code. You are provided with:
         // { All stuff is reactive }
@@ -26,25 +30,72 @@ export default {
         // Finally, let's make the canvas dirty!
         draw(ctx) {
             const l = this.$props.layout
-            const c = { x : l.width / 2, y : l.height / 2 }
-            ctx.lineWidth = 1
-            ctx.strokeStyle = 'gray'
+            const c = {
+                x: l.width / 2,
+                y: l.height / 2
+            }
+            ctx.lineWidth = 2
+            ctx.strokeStyle = 'black'
             ctx.fillStyle = '#ffea03'
+            // Strong Head
             ctx.beginPath()
-            ctx.arc(c.x, c.y, 50, 0, Math.PI * 2, true) // Outer circle
+            ctx.arc(c.x, c.y, 50, 0, Math.PI * 2, true)
             ctx.fill()
             ctx.stroke()
+            ctx.fillStyle = 'white'
+            ctx.strokeStyle = 'black'
+            // Wild Grin
+            ctx.lineWidth = 1
             ctx.beginPath()
             ctx.strokeStyle = 'black'
-            ctx.moveTo(c.x + 35, c.y)
-            ctx.arc(c.x, c.y, 35, 0, Math.PI , false)  // Mouth (clockwise)
-            ctx.moveTo(c.x - 10, c.y - 10)
-            ctx.fillStyle = '#ffea03'
-            ctx.arc(c.x - 15, c.y - 10, 5, 0, Math.PI * 2, true)  // Left eye
+            if (this.woo) ctx.arc(c.x, c.y, 42, -Math.PI * 0.1, Math.PI * 1.1)
+            let d = this.woo ? 0 : 0.15
+            let w = this.woo ? 2 : 0
+            ctx.arc(c.x, c.y - (27 - w), 42, Math.PI * 0.9 - d, +Math.PI * .1 + d, true)
+            if (this.woo) ctx.fill()
+            ctx.stroke()
+            ctx.beginPath()
+            ctx.lineWidth = 0.25
+            if (!this.woo) ctx.arc(c.x, c.y + 15, 25, Math.PI * 0.45, Math.PI * .6)
+            ctx.stroke()
+            ctx.lineWidth = 0.15
+            ctx.beginPath()
+            ctx.strokeStyle = 'black'
+            var s1 = Math.PI * .1
+            var s2 = Math.PI * 0.15
+
+            if (this.woo)
+                for (var k = 0; k < 0.85; k += 0.1) {
+                    ctx.arc(c.x, c.y - 25, 43,
+                        s1 + k * Math.PI,
+                        s1 + (k + 0.01) * Math.PI, false)
+                    ctx.arc(c.x, c.y, 42,
+                        s2 + k * Math.PI,
+                        s2 + (k + 0.01) * Math.PI, false)
+                }
+
+            ctx.stroke()
+            // Eagle eyes
+            ctx.beginPath()
+            ctx.fillStyle = 'black'
+            ctx.strokeStyle = 'black'
+            ctx.lineWidth = 1
+            ctx.moveTo(c.x - 17, c.y - 5)
+            ctx.arc(c.x - 17, c.y - 5, 3, 0, Math.PI * 2, true)
             ctx.fill()
-            ctx.moveTo(c.x + 20, c.y - 10)
-            ctx.arc(c.x + 15, c.y - 10, 5, 0, Math.PI * 2, true)  // Right eye
+            ctx.moveTo(c.x + 17, c.y - 5)
+            ctx.arc(c.x + 17, c.y - 5, 3, 0, Math.PI * 2, true)
             ctx.fill()
+            ctx.stroke()
+            ctx.lineCap = "round"
+            ctx.beginPath()
+            ctx.lineWidth = 5
+            if (this.woo) {
+                ctx.moveTo(c.x - 25, c.y - 15 + 2)
+                ctx.lineTo(c.x - 10, c.y - 7 + 2)
+                ctx.moveTo(c.x + 25, c.y - 20 + 2)
+                ctx.lineTo(c.x + 10, c.y - 7 + 2)
+            }
             ctx.stroke()
         },
 
@@ -55,12 +106,34 @@ export default {
         // indicator (let's say EMA),
         // just create a new overlay with the same type:
         // e.g. use_for() { return ['EMA'] }.
-        use_for() { return ['GRIN'] },
-        data_colors() { return ['yellow'] }
+        use_for() {
+            return ['GRIN']
+        },
+        data_colors() {
+            return ['yellow']
+        },
+        mousemove(e) {
+            const l = this.$props.layout
+            const c = {
+                x: l.width / 2,
+                y: l.height / 2
+            }
+            if (
+                (c.x - e.clientX) * (c.x - e.clientX) +
+                (c.y - e.clientY) * (c.y - e.clientY) <
+                42 * 42
+            ) {
+                this.woo = true
+            } else {
+                this.woo = false
+            }
+        },
     },
     data() {
         // Define internal setting & constants here
-        return {}
+        return {
+            woo: false
+        }
     }
 
 }
